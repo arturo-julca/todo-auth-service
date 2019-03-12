f7bcd31d8e2591f8b8dfc253e01e939ea87b6d0d
ArturoJulca-minted <arturo.julca@minted.com>
2019-03-12 17:31:25 -0500

    First commit

---
+++ src/main/java/com/avantica/todoauthservice/TodoAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.avantica.todoauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoAuthServiceApplication.class, args);
+	}
+
+}
