ba5ea4208d4a344718cc170c3fe30657325277e9
ArturoJulca-minted <arturo.julca@minted.com>
2019-03-13 02:04:22 -0500

    Added call to internal user service for user details

--- src/main/java/com/avantica/todoauthservice/TodoAuthServiceApplication.java
+++ src/main/java/com/avantica/todoauthservice/TodoAuthServiceApplication.java
@@ -2,12 +2,30 @@ package com.avantica.todoauthservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient 		// Enable eureka client.
+@EnableCircuitBreaker 	// Enable circuit breakers
 public class TodoAuthServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoAuthServiceApplication.class, args);
 	}
+}
 
+@Configuration
+class RestTemplateConfig {
+	
+	// Create a bean for restTemplate to call services
+	@Bean
+	@LoadBalanced		// Load balance between service instances running at different ports.
+	public RestTemplate restTemplate() {
+	    return new RestTemplate();
+	}
 }
